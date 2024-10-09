5fd770726dbbb7ed26f442ba92a51746e78d1bc4
Sebastian <sebassuarez103@gmail.com>
2024-10-09 18:50:29 -0500

    Inicializacion del proyecto, conexion con la base de datos, implementacion de mapper

---
+++ src/main/java/co/edu/uco/ubico/usuarios/init/UbicoUsuariosMsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.ubico.usuarios.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbicoUsuariosMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbicoUsuariosMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uco/ubico/usuarios/init/UbicoUsuariosMsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.ubico.usuarios.init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbicoUsuariosMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
